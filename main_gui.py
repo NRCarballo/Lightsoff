49ffc0d5f05ae0b41b9b65272fd76f20d79208ec
NRCarballo <nrcarballo@gmail.com>
2024-04-22 12:49:48 -0300

    update

--- main_gui.py
+++ main_gui.py
@@ -1,12 +1,14 @@
 from tkinter import *    
-from tkinter.messagebox import *
+from tkinter import messagebox
+
+
+def cmd():
+    messagebox.showinfo(message="Estamos trabajando en esto..")
+    print("Estamos trabajando en esto..")
 
 window = Tk()
 window.title ("LightsOff")
 #window.iconbitmap("img/light.ico")
 window.geometry("250x25")
-
-
-boton = Button(window, text="Shutdown lights").pack()
-
+boton = Button(window, command=cmd,  text="Shutdown lights").pack()
 window.mainloop()
